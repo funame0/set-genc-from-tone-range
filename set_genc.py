dd3f3e2067508f98934ee2563df72ea36632f5da
funame0 <huraltupe@gmail.com>
2023-10-16 11:50:19 +0900

    def genc_from_tone_range, calc_dist_from_range

--- set_genc.py
+++ set_genc.py
@@ -1,4 +1,6 @@
 import re
+import sys
+import yaml
 
 
 def spn2tone(spn):
@@ -12,3 +14,23 @@ def spn2tone(spn):
 
     tone = tonenames.index(str.upper(tonename)) + 12 * (int(octave) + 1)
     return tone
+
+
+def calc_dist_from_range(tone, lowest, highest):
+    if tone < lowest:
+        return lowest - tone
+    if tone > highest:
+        return highest - tone
+    return 0
+
+
+def genc_from_tone_range(tone: int, lowest_spn: str, highest_spn: str):
+    if not isinstance(tone, int):
+        raise TypeError("tone must be int.")
+
+    lowest_tone = spn2tone(lowest_spn)
+    highest_tone = spn2tone(highest_spn)
+
+    distance = calc_dist_from_range(tone, lowest_tone, highest_tone)
+    genc = int(distance / 12)
+    return genc
